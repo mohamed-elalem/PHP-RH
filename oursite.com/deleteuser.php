30a2848aa170087ec232dd86a52bb9ab510d0cce
Mai <maiosman016@gmail.com>
2017-02-28 17:17:12 +0200

    delete user home directory

--- oursite.com/deleteuser.php
+++ oursite.com/deleteuser.php
@@ -4,5 +4,7 @@
     $user_name= $_POST['delete_user'];
     //echo " $user_name";
     shell_exec("sudo userdel '$user_name'");
+    shell_exec("sudo rm -rf /home/'$user_name'");
+
     header('Location: index.php');
  ?>
