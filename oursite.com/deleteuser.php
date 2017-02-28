007e2d62077ec923f75f033e04727507279bc2f4
Mai <maiosman016@gmail.com>
2017-02-28 17:00:04 +0200

    update delete user file

--- oursite.com/deleteuser.php
+++ oursite.com/deleteuser.php
@@ -2,7 +2,7 @@
     //include('check_request.php');
     session_start();
     $user_name= $_POST['delete_user'];
-    echo " $user_name";
-    exec("sudo userdel '$user_name'");
+    //echo " $user_name";
+    shell_exec("sudo userdel '$user_name'");
     header('Location: index.php');
  ?>
