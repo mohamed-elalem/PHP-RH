de7c191984014e6e02fcb8c53389d774882e0e8a
Mai <maiosman016@gmail.com>
2017-03-01 13:28:39 +0200

    add style for update group

--- oursite.com/updategroup.php
+++ oursite.com/updategroup.php
@@ -16,13 +16,35 @@ if(isset($_POST['edit']) && isset($_POST['group_name_f'])){
  ?>
 <html>
     <header>
-        <title> update_group </title>
+        <head>
+         <title> update_group </title>
+         <meta charset="utf-8">
+         <meta name="viewport" content="width=device-width, initial-scale=1">
+         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        </head>
     </header>
     <body>
+      <div class="jumbotron">
+        <h1>Updating Group</h1>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4">
+
+          </div>
+          <div class="col-md-4">
         <form action="updategroup.php" method="POST">
+          <div class="form-group">
             <label> Enter new group name : </label>
-            <input type="text" name="group_name_f">
-            <button type="submit" name="edit">Edit</button>
+            <input class="form-control" type="text" name="group_name_f">
+          </div>
+            <button type="submit" name="edit" class="btn btn-primary pull-right">Edit</button>
         </form>
+      </div>
+    </div>
+  </div>
+    </div>
     </body>
 </html>
