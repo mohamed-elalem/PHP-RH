73fa2a2e820b1a60a8d211c06c0aaac0c2f11e38
Mai <maiosman016@gmail.com>
2017-03-01 15:24:29 +0200

    edited

--- oursite.com/creategroup.php
+++ oursite.com/creategroup.php
@@ -1,40 +1,51 @@
 <?php
-//include('check_request.php');
 
-if(isset($_POST['create']) && isset($_POST['group_name_f']) && isset($_POST['group_id_f'])){
-    $group_Name= $_POST['group_name_f'];
-    $group_id= $_POST['group_id_f'];
-    if(!empty($group_Name) && !empty($group_id)){
-        $result  = explode(PHP_EOL, shell_exec("cat /etc/group | grep '$group_Name' "));
-        echo "hi";
-        print_r($result);
-        if(empty($result)){
-            if(empty($result)){
-                exec("sudo groupadd -g $group_Name $group_id ");
-                //header('Location: groups.php');
-            }else{
-                echo "group ID  $group_id already exists";
-            }
-        }else{
-            echo "group $group_Name already exists";
-        }
-    }else{
-        echo "Please Enter required data";
-    }
+    session_start();
+    $group_name= $_POST['group_name'];
+  if(isset($_POST['create_group'])){
+    exec("sudo groupadd '$group_name'",$output,$ret);
+  if($ret==0)
+  {
+    header('Location: groups.php');
+  }
+  
 }
 
- ?>
+?>
 <html>
-    <header>
-        <title> create_group </title>
-    </header>
-    <body>
-        <form action="creategroup.php" method="POST">
-            <label> Enter group name : </label>
-            <input type="text" name="group_name_f"><br>
-            <label> Enter group id : </label>
-            <input type="text" name="group_id_f">
-            <button type="submit" name="create">Create</button>
-        </form>
-    </body>
+<head>
+  <title>Bootstrap Example</title>
+ <meta charset="utf-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+ <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+ <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+  <body>
+
+<div class="jumbotron">
+  <h1>Creating Group</h1>
+</div>
+<div class="container">
+  <div class="row">
+    <div class="col-md-4">
+
+    </div>
+    <div class="col-md-4">
+      <form method="POST" action="create_group.php">
+
+          <div class="form-group">
+                <label for="ex3">Creating Group</label>
+                <input class="form-control" id="ex3" name="group_name" type="text">
+            </div>
+              <input type="submit" name="create_group" class="btn btn-primary pull-right" value="Create Group">
+
+          </form>
+
+    </div>
+  </div>
+</div>
+  </div>
+  </body>
+
 </html>
