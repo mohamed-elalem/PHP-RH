bfc1e7ccd2ef0b37137f83ca5ce4617b39d4c9b2
maiosman <maiosman016@gmail.com>
2017-03-03 21:43:04 +0200

    adding new log files

--- oursite.com/log/LogsFunctions.php
+++ oursite.com/log/LogsFunctions.php
@@ -1,13 +1,17 @@
 <?php
 
+//session_start();
 
-
-function errlog($gName,$uName,$message){
+if (isset($_SESSION['username']) && isset($_SESSION['groupname']) && isset($_SESSION['projectNum'])){
+function errlog($message){
 	//$u =exec('whoami');
+	$uName=$_SESSION['username'];
+	$gName=$_SESSION['groupname'];
+	$projNum=$_SESSION['projectNum'];
 	$app="PhpProject";
 	$gName = preg_replace('/\s+/', '', $gName);
 	$uName = preg_replace('/\s+/', '', $uName);
-	$log=$gName." ".$uName." **ERROR** ".$message;
+	$log=$projNum." ".$gName.":".$uName." **ERROR** ".$message;
 //	exec("logger -i -t '$u' -p local2.err '$log'");
          //define_syslog_variables();
         openlog($app, LOG_NDELAY | LOG_PID , LOG_LOCAL2);
@@ -17,12 +21,15 @@ function errlog($gName,$uName,$message){
         
 }
 
-function warnlog($gName,$uName,$message){
+function warnlog($message){
 	//$u =exec('whoami');
 	$app="PhpProject";
+	$uName=$_SESSION['username'];
+	$gName=$_SESSION['groupname'];
+	$projNum=$_SESSION['projectNum'];
 	$gName = preg_replace('/\s+/', '', $gName);
 	$uName = preg_replace('/\s+/', '', $uName);
-	$log=$gName." ".$uName." **WARNING** ".$message;
+	$log=$projNum." ".$gName.":".$uName." **WARNING** ".$message;
 //	exec("logger -i -t '$u' -p local3.warn '$log'");
         //define_syslog_variables();
         openlog($app, LOG_NDELAY | LOG_PID , LOG_LOCAL3);
@@ -31,20 +38,23 @@ function warnlog($gName,$uName,$message){
 }
 
 
-function infolog($gName,$uName,$message,$infoType){
+function infolog($message,$infoType){
 	//$u =exec('whoami');
 	$app="PhpProject";
+	$uName=$_SESSION['username'];
+	$gName=$_SESSION['groupname'];
+	$projNum=$_SESSION['projectNum'];
 	$gName = preg_replace('/\s+/', '', $gName);
 	$uName = preg_replace('/\s+/', '', $uName);
 	$infoType = preg_replace('/\s+/', '', $infoType);
-	$log=$gName." ".$uName." **".$infoType."** ".$message;
+	$log=$projNum." ".$gName.":".$uName." **".$infoType."** ".$message;
 	//exec("logger -i -t '$u' -p local4.info '$log'");
          //define_syslog_variables();
         openlog($app, LOG_NDELAY | LOG_PID , LOG_LOCAL4);
         syslog(LOG_INFO, $log);
         closelog();
 }
-
+}
 
 
 
